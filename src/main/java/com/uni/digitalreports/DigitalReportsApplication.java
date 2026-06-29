9dbf98a54460d7a1ecbfda9c6d8d3557ef7bbb92
MilllerDev <bailonalvarezmiller12@gmail.com>
2026-06-28 22:27:03 -0500

    Initial commit

---
+++ src/main/java/com/uni/digitalreports/DigitalReportsApplication.java
@@ -0,0 +1,13 @@
+package com.uni.digitalreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalReportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uni/digitalreports/DigitalReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.digitalreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalReportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
